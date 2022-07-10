c68c7479a4d19f08f712d239621967cc10bfc0aa
Christian Diemers <christian.diemers@gmail.com>
2022-07-10 11:05:59 +0200

    Add puppeteer dependency

--- automate-browser/index.js
+++ automate-browser/index.js
@@ -1,13 +1,21 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
+  const puppeteer = require("puppeteer");
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto("https://danube-webshop.herokuapp.com");
+  await browser.close();
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = name
+    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
